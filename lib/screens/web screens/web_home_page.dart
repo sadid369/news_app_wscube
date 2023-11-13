a440f9413718e15cd99632d1aaff508b96819bac
sadid369 <sadid.jones@gmail.com>
2023-11-13 19:58:43 +0600

    UPDATED

--- lib/screens/web screens/web_home_page.dart
+++ lib/screens/web screens/web_home_page.dart
@@ -74,7 +74,7 @@ class _WebHomePageState extends State<WebHomePage> {
               ],
             ),
             SizedBox(
-              height: 25,
+              height: 7,
             ),
             //Latest News
             Container(
@@ -87,7 +87,7 @@ class _WebHomePageState extends State<WebHomePage> {
               ),
             ),
             SizedBox(
-              height: 20,
+              height: 5,
             ),
             //News Category Row
             Row(
@@ -108,16 +108,195 @@ class _WebHomePageState extends State<WebHomePage> {
                         ))
                     .toList()),
             SizedBox(
-              height: 20,
+              height: 10,
             ),
             //main Contain
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                          width: double.infinity,
+                          height: 150,
+                          child: Image.network(
+                            'https://plus.unsplash.com/premium_photo-1674375039817-cb0a39ad2f98?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aWNlJTIwbW91bnRhaW58ZW58MHx8MHx8fDA%3D',
+                            fit: BoxFit.cover,
+                          )),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Unrecognized genius',
+                          style: GoogleFonts.poppins(fontSize: 24),
+                        ),
+                      ),
+                      Text(
+                        'fficia eiusmod quis esse id est aliquip. Cillum eiusmod proident aute anim tempor deserunt tempor amet mollit nostrud. Id eiusmod Lorem officia est occaecat aliquip nisi.',
+                        style: GoogleFonts.poppins(fontSize: 8),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                  'https://images.unsplash.com/photo-1587778082149-bd5b1bf5d3fa?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5NDUyNDk0fHxlbnwwfHx8fHw%3D',
+                                ),
+                              ),
+                              color: Colors.blue,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Wode Warren',
+                                style: GoogleFonts.poppins(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10,
+                                ),
+                              ),
+                              Text(
+                                'Newsman',
+                                style: GoogleFonts.poppins(
+                                  fontSize: 7,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      Divider(),
+                      Text(
+                        'Photography',
+                        style: GoogleFonts.poppins(
+                          fontSize: 15,
+                        ),
+                      ),
+                      Text(
+                        ' est esse. Irure enim sit qui quis nulla. Magna ad aliqua irure nulla mollit ex ex deserunt aliqua tempor pariatur sit excepteur proident. In nulla deserunt esse nostrud. Laboris sit exercitation incididunt aute ex duis elit aliquip laboris nulla aliqua est reprehenderit aliqua.',
+                        style: GoogleFonts.poppins(
+                          fontSize: 10,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                    flex: 2,
                     child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                            width: double.infinity,
+                            height: 190,
+                            child: Image.network(
+                              'https://plus.unsplash.com/premium_photo-1674375039817-cb0a39ad2f98?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aWNlJTIwbW91bnRhaW58ZW58MHx8MHx8fDA%3D',
+                              fit: BoxFit.cover,
+                            )),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            'Unrecognized genius',
+                            style: GoogleFonts.poppins(fontSize: 24),
+                          ),
+                        ),
+                        Text(
+                          'fficia eiusmod quis esse id est aliquip. Cillum eiusmod proident aute anim tempor deserunt tempor amet mollit nostrud. Id eiusmod Lorem officia est occaecat aliquip nisi.',
+                          style: GoogleFonts.poppins(fontSize: 8),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              width: 40,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(
+                                    'https://images.unsplash.com/photo-1587778082149-bd5b1bf5d3fa?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5NDUyNDk0fHxlbnwwfHx8fHw%3D',
+                                  ),
+                                ),
+                                color: Colors.blue,
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Wode Warren',
+                                  style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 10,
+                                  ),
+                                ),
+                                Text(
+                                  'Newsman',
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 7,
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        Divider(),
+                        Text(
+                          'Photography',
+                          style: GoogleFonts.poppins(
+                            fontSize: 15,
+                          ),
+                        ),
+                        Text(
+                          ' est esse. Irure enim sit qui quis nulla. Magna ad aliqua irure nulla mollit ex ex deserunt aliqua tempor pariatur sit excepteur proident. In nulla deserunt esse nostrud. Laboris sit exercitation incididunt aute ex duis elit aliquip laboris nulla aliqua est reprehenderit aliqua.',
+                          style: GoogleFonts.poppins(
+                            fontSize: 10,
+                          ),
+                        )
+                      ],
+                    )),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                    child: Column(
                   children: [
-                    SizedBox(width: 100, height: 150, child: Image.network('')),
+                    SizedBox(
+                        width: double.infinity,
+                        height: 150,
+                        child: Image.network(
+                          'https://plus.unsplash.com/premium_photo-1674375039817-cb0a39ad2f98?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aWNlJTIwbW91bnRhaW58ZW58MHx8MHx8fDA%3D',
+                          fit: BoxFit.cover,
+                        )),
                     SizedBox(
                       height: 10,
                     ),
@@ -129,16 +308,25 @@ class _WebHomePageState extends State<WebHomePage> {
                       ),
                     ),
                     Text(
-                      'In sunt enim minim do proident nisi reprehenderit sit. Quis ut est laborum velit laborum velit officia eiusmod quis esse id est aliquip. Cillum eiusmod proident aute anim tempor deserunt tempor amet mollit nostrud. Id eiusmod Lorem officia est occaecat aliquip nisi.',
+                      'fficia eiusmod quis esse id est aliquip. Cillum eiusmod proident aute anim tempor deserunt tempor amet mollit nostrud. Id eiusmod Lorem officia est occaecat aliquip nisi.',
                       style: GoogleFonts.poppins(fontSize: 8),
+                    ),
+                    SizedBox(
+                      height: 10,
                     ),
                     Row(
                       // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Container(
-                          width: 50,
-                          height: 50,
+                          width: 40,
+                          height: 40,
                           decoration: BoxDecoration(
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                'https://images.unsplash.com/photo-1587778082149-bd5b1bf5d3fa?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5NDUyNDk0fHxlbnwwfHx8fHw%3D',
+                              ),
+                            ),
                             color: Colors.blue,
                             shape: BoxShape.circle,
                           ),
@@ -146,36 +334,40 @@ class _WebHomePageState extends State<WebHomePage> {
                         SizedBox(
                           width: 5,
                         ),
-                        Container(
-                          // width: 200,
-                          alignment: Alignment.topLeft,
-                          // padding: EdgeInsets.symmetric(horizontal: 5),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Wode Warren',
-                                style: GoogleFonts.poppins(),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Wode Warren',
+                              style: GoogleFonts.poppins(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
                               ),
-                              Text(
-                                'Newsman',
-                                style: GoogleFonts.poppins(),
-                              )
-                            ],
-                          ),
+                            ),
+                            Text(
+                              'Newsman',
+                              style: GoogleFonts.poppins(
+                                fontSize: 7,
+                              ),
+                            )
+                          ],
                         )
                       ],
+                    ),
+                    Divider(),
+                    Text(
+                      'Photography',
+                      style: GoogleFonts.poppins(
+                        fontSize: 15,
+                      ),
+                    ),
+                    Text(
+                      ' est esse. Irure enim sit qui quis nulla. Magna ad aliqua irure nulla mollit ex ex deserunt aliqua tempor pariatur sit excepteur proident. In nulla deserunt esse nostrud. Laboris sit exercitation incididunt aute ex duis elit aliquip laboris nulla aliqua est reprehenderit aliqua.',
+                      style: GoogleFonts.poppins(
+                        fontSize: 10,
+                      ),
                     )
                   ],
-                )),
-                Expanded(
-                    flex: 2,
-                    child: Column(
-                      children: [],
-                    )),
-                Expanded(
-                    child: Column(
-                  children: [],
                 )),
               ],
             )
